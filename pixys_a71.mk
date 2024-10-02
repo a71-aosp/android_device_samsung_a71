0eb112dc1c7b7da4035330392ffc845ad0c58ce6
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-10-02 17:58:07 +0500

    a71

--- pixys_a71.mk
+++ pixys_a71.mk
@@ -17,6 +17,10 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A715
 PRODUCT_MANUFACTURER := samsung
 
+# PixysOS flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_CALL_RECORDING := true
+
 # Use the latest approved GMS identifiers
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
