ff8d133dd83c0e36593f6722c248a78162b12a0a
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-10-04 21:07:33 +0500

    a71: ok

--- blaze_a71.mk
+++ blaze_a71.mk
@@ -11,7 +11,7 @@ $(call inherit-product, device/samsung/a71/device.mk)
 $(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_a71
+PRODUCT_NAME := blaze_a71
 PRODUCT_DEVICE := a71
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A715
