40a37216b221327ec0be5fea17efa524e0a74661
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-10-04 21:07:21 +0500

    a71: BringUp Blaze

--- blaze_a71.mk
+++ blaze_a71.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/samsung/a71/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_a71
@@ -16,6 +16,19 @@ PRODUCT_DEVICE := a71
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A715
 PRODUCT_MANUFACTURER := samsung
+
+# Blaze Specific
+BLAZE_MAINTAINER := WhyStarlix
+
+# Gapps Flags
+WITH_GAPPS := true
+
+# Extras
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Use the latest approved GMS identifiers
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
